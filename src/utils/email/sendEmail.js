06ee367717d84cb701a6880c2ac73a7fb7d48c41
Taryana <hatakekakasih2002@gmail.com>
2022-11-15 15:59:27 +0700

    fix Deploy

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -29,9 +29,7 @@ const sendEmail = async (dataEmail) =>{
         //     }
         // })
           const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true, // true for 465, false for other ports
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER, // generated ethereal user
                 pass: 'taoeqyuovqivguzd' // generated ethereal password
