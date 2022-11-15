0936e789afdb18b0a42e5e91ed524a87612bc984
Taryana <hatakekakasih2002@gmail.com>
2022-11-15 14:50:48 +0700

    fix Deploy

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -17,8 +17,6 @@ const sendEmail = async (dataEmail) =>{
         const accessToken = await oAuth2Client.getAccessToken();
         const transporter = nodemailer.createTransport({
             service: 'gmail',
-            // port: 456,
-            // secure: true, 
             auth: {
                 type: 'OAuth2',
                 user: process.env.EMAIL_USER,
