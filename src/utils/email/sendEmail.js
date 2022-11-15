ffe1315759fb6c7cb0b71d7d6bacf35c9a1a6062
Taryana <hatakekakasih2002@gmail.com>
2022-11-15 15:05:19 +0700

    fix Deploy

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -15,19 +15,28 @@ oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN})
 const sendEmail = async (dataEmail) =>{
     try {
         const accessToken = await oAuth2Client.getAccessToken();
-        const transporter = nodemailer.createTransport({
-            service: 'smtp.gmail.com',
-            port: 456,
-            secure: true, 
+        // const transporter = nodemailer.createTransport({
+        //     service: 'smtp.gmail.com',
+        //     port: 456,
+        //     secure: true, 
+        //     auth: {
+        //         type: 'OAuth2',
+        //         user: process.env.EMAIL_USER,
+        //         clientId: process.env.GOOGLE_CLIENT_ID,
+        //         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        //         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        //         accessToken,
+        //     }
+        // })
+          const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // true for 465, false for other ports
             auth: {
-                type: 'OAuth2',
-                user: process.env.EMAIL_USER,
-                clientId: process.env.GOOGLE_CLIENT_ID,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-                accessToken,
+                user: process.env.EMAIL_USER, // generated ethereal user
+                pass: 'asdfgyuiop09876' // generated ethereal password
             }
-        })
+            })
         transporter
         .sendMail(dataEmail)
         .then((info) =>{
