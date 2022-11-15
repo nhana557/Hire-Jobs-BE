85acaa9cfe4b60decb8d44abbb07c806b3fd7679
Taryana <hatakekakasih2002@gmail.com>
2022-11-15 16:20:46 +0700

    fix Deploy

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -1,33 +1,19 @@
 require('dotenv').config();
 
-const { google } = require('googleapis')
+// const { google } = require('googleapis')
 const nodemailer = require('nodemailer')
 
 
-const oAuth2Client  = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.REDIRECT_URI,
-);
+// const oAuth2Client  = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     process.env.REDIRECT_URI,
+// );
 
-oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN})
+// oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN})
 
 const sendEmail = async (dataEmail) =>{
     try {
-        const accessToken = await oAuth2Client.getAccessToken();
-        // const transporter = nodemailer.createTransport({
-        //     service: 'smtp.gmail.com',
-        //     port: 456,
-        //     secure: true, 
-        //     auth: {
-        //         type: 'OAuth2',
-        //         user: process.env.EMAIL_USER,
-        //         clientId: process.env.GOOGLE_CLIENT_ID,
-        //         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        //         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        //         accessToken,
-        //     }
-        // })
           const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
